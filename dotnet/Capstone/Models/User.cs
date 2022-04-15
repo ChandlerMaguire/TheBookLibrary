a6e6715095ec5373252c35d1779f971844433ed3
Jake Nelson <jakenelson1050@gmail.com>
2022-04-15 16:41:21 -0400

    moved update last search method and added return of that value

--- dotnet/Capstone/Models/User.cs
+++ dotnet/Capstone/Models/User.cs
@@ -7,6 +7,7 @@
         public string PasswordHash { get; set; }
         public string Salt { get; set; }
         public string Role { get; set; }
+        public string LastSearch { get; set; }
     }
 
     /// <summary>
