f87764b2d1ff67c85423f70a652faf57d978c5a5
Sean Bamford <sean.bamford.sb@gmail.com>
2022-04-19 10:47:16 -0400

    transferring

--- dotnet/Capstone/DAO/CommentSqlDao.cs
+++ dotnet/Capstone/DAO/CommentSqlDao.cs
@@ -53,9 +53,9 @@ namespace Capstone.DAO
                 {
                     conn.Open();
                     SqlCommand cmd = new SqlCommand(sqlAddComment, conn);
-                    cmd.Parameters.AddWithValue("@post_id", postId);
-                    cmd.Parameters.AddWithValue("@commentor_id", commentorId);
-                    cmd.Parameters.AddWithValue("@comment_text", commentToAdd.CommentText);
+                    cmd.Parameters.AddWithValue("@postId", postId);
+                    cmd.Parameters.AddWithValue("@commentorId", commentorId);
+                    cmd.Parameters.AddWithValue("@commentText", commentToAdd.CommentText);
                     
 
                     int count = cmd.ExecuteNonQuery();
